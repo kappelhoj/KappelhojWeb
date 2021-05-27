47c1580205cf21d51b5a85d1494506033f815dda
Arvid Langsø <a.kappelhoj@gmail.com>
2021-05-27 21:16:59 +0200

    feat: Simple top navigation and multipaging (#7)
    * Added multipaging
    
    * Made a page structure
    
    * Add basic navigation bar
    
    * Added global CSS
    
    Co-authored-by: ArvidLangsoe <arvid.k.langsoe@live.dk>

---
+++ Web/main/src/components/Structure/DefaultPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page">
+    <div class="header">
+      <NavBar/>
+    </div>
+    <div class="main">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '../../components/Structure/Navigation/NavBar.vue'
+
+export default {
+  name: 'DefaultPage',
+    components: {
+    NavBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .page{
+    display: grid;
+    grid-template-columns: minmax(10px,auto) minmax(0,1280px) minmax(10px,auto);
+    grid-template-rows: 3em auto;
+    grid-template-areas: 
+    " . header ."
+    ". main .";
+    text-align: center;
+    color: var(--main-text-color);
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  .header{
+    grid-area: header;
+  }
+
+  .main{
+    grid-area: main;
+  }
+</style>
---
+++ Web/main/src/components/Structure/Navigation/NavItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="nav-item">
+    <h3>
+      <a :href=path>{{title}}</a>
+    </h3>
+  </div>
+</template>
+
+
+<script>
+export default{
+  name: 'NavItem',
+  props:{
+    title:{
+      type: String,
+      required: true
+    },
+    path:{
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .nav-item{
+    margin-left: 20px;
+  }
+  
+  a{
+    text-decoration: none;
+    color: var(--secondary-text-color);
+  }
+  a:hover{
+    color: var(--main-text-color);
+  }
+</style>
