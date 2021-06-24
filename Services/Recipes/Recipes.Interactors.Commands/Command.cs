3130bc31c6324011b8a317cccf1ea1660c480a7c
ArvidLangsoe <arvid.k.langsoe@live.dk>
2021-06-24 21:12:20 +0200

    Project renaming

---
+++ Services/Recipes/Recipes.Interactors.Commands/Command.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.Interactors.Commands
+{
+    public abstract class Command<T> : IRequest<T> 
+    {
+        public Guid CorrelationId { get; set; }
+    }
+}
