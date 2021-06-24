02e0d20631517ad7fd69b1acae0b45d000a5d2c1
ArvidLangsoe <arvid.k.langsoe@live.dk>
2021-06-24 21:27:31 +0200

    Another restructure

---
+++ Services/Recipes/FoodPlanner.Commands.Interactors/Command.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace FoodPlanner.Commands.Interactors
+{
+    public abstract class Command<T> : IRequest<T> 
+    {
+        public Guid CorrelationId { get; set; }
+    }
+}
---
+++ Services/Recipes/FoodPlanner.Commands.Interactors/Recipes/AddRecipeCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoodPlanner.Commands.Interactors.Recipes
+{
+    public class AddRecipeCommand : Command<Response<string>>
+    {
+    }
+
+    public class AddRecipeHandler : IRequestHandler<AddRecipeCommand, Response<string>>
+    {
+
+        Task<Response<string>> IRequestHandler<AddRecipeCommand, Response<string>>.Handle(AddRecipeCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
