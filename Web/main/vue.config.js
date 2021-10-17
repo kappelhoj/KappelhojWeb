e03f55cb84b39c2f663e5b54e07b793d06d06df8
ArvidLangsoe <arvid.k.langsoe@live.dk>
2021-10-17 16:00:17 +0200

    build: Added publicPath config

--- Web/main/vue.config.js
+++ Web/main/vue.config.js
@@ -1,16 +1,19 @@
 module.exports ={
-    pages:{
-        "index":{
-            entry: "./src/pages/Home/main.js",
-            template: "public/index.html",
-            title: "Home",
-            chunks: ["chunk-vendors", "chunk-common", "index"]
-        },
-        "dev":{
-            entry: "./src/pages/Dev/main.js",
-            template: "public/index.html",
-            title: "Dev",
-            chunks: ["chunk-vendors", "chunk-common", "dev"]
-        },
-    }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/KappelhojWeb/'
+    : '/',
+  pages:{
+    "index":{
+      entry: "./src/pages/Home/main.js",
+      template: "public/index.html",
+      title: "Home",
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "dev":{
+      entry: "./src/pages/Dev/main.js",
+      template: "public/index.html",
+      title: "Dev",
+      chunks: ["chunk-vendors", "chunk-common", "dev"]
+    },
+  }
 }
