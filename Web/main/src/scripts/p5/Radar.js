50b269ab6da9b2565022c86c0efcb0cebacb66ad
ArvidLangsoe <arvid.k.langsoe@live.dk>
2022-02-10 19:47:56 +0100

    Random ball 😃

--- Web/main/src/scripts/p5/Radar.js
+++ Web/main/src/scripts/p5/Radar.js
@@ -1,41 +1,35 @@
 let p5;
+let bubble;
 export function main(p5Init){
   p5 = p5Init; 
-  const defaultSpeed = 2;
-  var speed = defaultSpeed;    
-  var posX = 0;
   
   // NOTE: Set up is here   
   p5.setup = () => {      
     var canvas = p5.createCanvas(500, 500);
-    canvas.parent("p5Canvas");    
-    p5.ellipse(p5.width / 2, p5.height / 2, 500, 500);    
+    canvas.parent("p5Canvas");
+    bubble =  new Bubble();
   }     
   // NOTE: Draw is here
   p5.draw = () => {      
     p5.background(0);
-    const degree = p5.frameCount * 3;      
-    const y = p5.sin(p5.radians(degree)) * 50;
-    
     p5.push();
-    p5.translate(0, p5.height / 2);
-    p5.ellipse(posX, y, 50, 50);
+    bubble.display();
     p5.pop();
-    posX += speed;
-      
-    if (posX > p5.width) {    
-      speed = -defaultSpeed;      
-    }
-    else if(posX < 0){
-      speed = defaultSpeed;
-    }
   } 
 
   p5.mouseClicked = () => {
-    speed = reverse_speed(speed);
+    bubble = new Bubble();
   }
 }
 
-function reverse_speed(speed){
-  return speed*-1;
+class Bubble {
+  constructor(){
+    this.x = p5.random(p5.width);
+    this.y = p5.random(p5.height);
+    this.diameter = 50;
+  }
+
+  display(){
+    p5.ellipse(this.x, this.y, this.diameter, this.diameter);    
+  }
 }
